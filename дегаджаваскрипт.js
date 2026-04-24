f26641bbf0002d5aedfffc39f386d7f5d9adc07d
ponomaryova291-create <ponomaryova291@yandex.ru>
2026-04-24 16:38:30 +0500

    Add files via upload

---
+++ дегаджаваскрипт.js
@@ -0,0 +1,17 @@
+const IMAGE_SRCS = [
+  "https://raw.githubusercontent.com/ponomaryova291-create/T-shirt/refs/heads/main/111.jpg",
+  "https://raw.githubusercontent.com/ponomaryova291-create/T-shirt/refs/heads/main/222.jpg",
+  "https://raw.githubusercontent.com/ponomaryova291-create/T-shirt/refs/heads/main/333.jpg",
+  "https://raw.githubusercontent.com/ponomaryova291-create/T-shirt/refs/heads/main/444.jpg",
+  "https://raw.githubusercontent.com/ponomaryova291-create/T-shirt/refs/heads/main/555.jpg",
+  "https://raw.githubusercontent.com/ponomaryova291-create/T-shirt/refs/heads/main/666.png"
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+  "ПЕРЕД РЕПЕТИЦИЕЙ",
+  "УРОК ТАНЦЕВ",
+  "ГОЛУБЫЕ ТАНЦОВЩИЦЫ",
+  "ТАНЦОВЩИЦЫ В РОЗОВОМ",
+  "РЕПЕТИЦИЯ БАЛЕТА",
+  "ЗВЕЗДА"
+];
